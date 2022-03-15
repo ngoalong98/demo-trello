c2fe1f4aa8a25fee7d152f6848c2cc2248ff4b99
ngoalong98 <longphv98@gmail.com>
2022-03-15 11:04:13 +0700

    Initial Sample Data

--- src/components/BoardContent/BoardContent.js
+++ src/components/BoardContent/BoardContent.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { isEmpty} from 'lodash';
 import './BoardContent.scss';
 import Column from 'components/Column/Column';
+import {initialData} from 'actions/initialData';
+import { mapOrder} from 'utilities/sorts';
 
 
 function BoardContent() {
+    const [board, setBoard] = useState({});
+    const [columns, setColumns] = useState([]);
+
+    useEffect(() => {
+        const boardFromDB = initialData.boards.find(board => board.id === 'board-1')
+        if(boardFromDB) {
+            setBoard(boardFromDB);
+
+            setColumns(mapOrder(boardFromDB.columns, boardFromDB.columnOrder, 'id'));
+        }
+    }, []);
+
+    if(isEmpty(board)) {
+        return <div className="not-found">Board not found!</div>
+    }
+
     return (
         <div className="board-content">
-            <Column />
+            {columns.map((column, index) => <Column key={index} column = {column} />)}
         </div>
     );
 }
--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import './Column.scss';
-import Task from 'components/Task/Task';
+import Card from 'components/Card/Card';
+import { mapOrder} from 'utilities/sorts';
 
 
-function Column() {
+function Column(props) {
+    const {column} = props;
+    const cards = mapOrder(column.cards, column.cardOrder, 'id');
+
     return (
         <div className="column">
-            <header>Header</header>
-            <ul className="task-list">
-                <Task />
-                <li className="task-item"> test demo trello </li>
-                <li className="task-item"> test demo trello </li>
-                <li className="task-item"> test demo trello </li>
-                <li className="task-item"> test demo trello </li>
+            <header>{column.title}</header>
+            <ul className="card-list">
+                {cards.map((card, index) =>  <Card key={index} card={card} /> )}
             </ul>
             <footer>Footer</footer>
         </div>
